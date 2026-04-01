9bb52abd0d87c88dd3ea50e5c9618403e928cf00
Aman kumar <amankumarsingh139@gmail.com>
2026-04-01 19:26:35 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+import os
+app = Flask(__name__)
+metrics = PrometheusMetrics(app, path="/metrics")
+
+
+@app.route("/")
+def home():
+    return "Welcome toCICD pipeline on AWS"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
